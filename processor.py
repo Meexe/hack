32e9f20069546cc906ec4d1217bb696f53e41aa4
Max Pak <maxpak99@gmail.com>
2018-04-21 02:13:23 +0300

    init

--- processor.py
+++ processor.py
@@ -1,4 +1,5 @@
 from random import randint
+from uuid import uuid4
 
 
 class Client(object):
@@ -10,10 +11,22 @@ class Client(object):
 
 class Game(object):
 
+    # ToDo описать здесь игровой сервер
+
     def __init__(self, player1, player2):
-        self.players = {'player1': player1,
-                        'player2': player2}
-        self.current_turn = [1, randint(0, 1)]
+        self.player1 = player1
+        self.player2 = player2
+        self.current_turn = [1, bool(randint(0, 1))]
+        self.methods = {''}
+
+    def process(self, data):
+        pass
+
+    def next_turn(self):
+        self.current_turn[0] += 1
+
+    def hand_turn(self):
+        self.current_turn[1] = not self.current_turn[1]
 
 
 class Processor(object):
@@ -23,7 +36,7 @@ class Processor(object):
         self.methods = {'ready': self.on_ready,
                         'game': self.process_game
                         }
-        self.games = list()
+        self.games = dict()
         self.limit = limit
 
     def process(self, data, addr):
@@ -37,11 +50,13 @@ class Processor(object):
         self.queue.append(client)
 
     def process_game(self, data, addr):
-        pass
+        game = self.games[data.pop('game_id')]
+        game.process(data)
 
     def start_game(self):
         if len(self.games) < self.limit & len(self.queue) >= 2:
             player1 = self.queue.pop(0)
             player2 = self.queue.pop(0)
+            game_id = uuid4()
             game = Game(player1, player2)
-            self.games.append(game)
+            self.games.update({game_id: game})
